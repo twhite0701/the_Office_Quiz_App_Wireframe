d1dcc75a0a6f8748436fe713a7be8c50e7dc034e
twhite0701 <52742917+twhite0701@users.noreply.github.com>
2019-10-26 12:04:50 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// constant to store question data
+
+  
+// variables to store the quiz score and question number information
+
+  
+// create reusable function to generate questions
+
+  
+// create reusable functions to track question #
+// create reusable functions to track score # 
+  
+
+// create reusable function to track if their answer is correct
+    // if correct increment their score by 1 
+// create reusable function to track if their answer is correct
+  
+// function to generate next question 
+  
+// generates the next question
+// generate end of quiz reusable function for how they did in 3 categories
+    // needs to include conditional statement to determine which category user landed in
+  
+// option to restart quiz
+  
+// create call back function to call on all of our functions
